cf4eccdd1b23b510dab6a732e0744ab5dcee6819
murata <murata@kmc.gr.jp>
2016-10-14 13:24:23 +0900

    report 1

---
+++ readme.md
@@ -0,0 +1,9 @@
+# requirements
+```sh
+$ pip3 install -r reqirements.txt
+```
+
+# example
+```sh
+$ python3 svm.py sample_data/sample_circle.dat -m gauss --plot
+```
--- svm.py
+++ svm.py
@@ -31,7 +31,7 @@ def solve(x, y, kernel):
     x = np.array(x)
     y = np.array(y)
     n = len(y)
-    assert(n == len(x))
+    assert n == len(x)
     P = cvxopt.matrix([[0.0] * n] * n)
     for k in range(n):
         for l in range(n):
@@ -40,16 +40,14 @@ def solve(x, y, kernel):
     G = cvxopt.spdiag([-1.0] * n)
     h = cvxopt.matrix([0.0] * n)
     A = cvxopt.matrix([[0.0] * 1] * n)
-    for k in range(n):
-        A[0, k] = y[k]
+    A[0, :] = y
     b = cvxopt.matrix(0.0)
     sol = cvxopt.solvers.qp(P, q, G, h, A, b, options={"show_progress": False})
     a = np.array([(x if x > 0.0000001 else 0) for x in sol["x"]])
     ok_indexes = [i for i in range(n) if abs(a[i]) > 0]
     ok_coes = [(a[i] * y[i], x[i]) for i in ok_indexes]
-
-    def kernel_dot_to_w(n_x):
-        return sum([co[0] * kernel(co[1], n_x) for co in ok_coes])
+    kernel_dot_to_w = lambda n_x: sum(
+        [co[0] * kernel(co[1], n_x) for co in ok_coes])
     max_index = a.argmax()
     theta = kernel_dot_to_w(x[max_index]) - y[max_index]
     print("α : " + str(a))
@@ -66,20 +64,10 @@ def solve(x, y, kernel):
     return f
 
 
-def plot_core(x1s, x2s, cs, ss, markers, labels):
-    for i in range(len(x1s)):
-        plt.scatter(x1s[i], x2s[i], c=cs[i], s=ss[i],
-                    marker=markers[i], label=labels[i])
-    plt.grid(True)
-    plt.legend(loc='upper left')
-    plt.savefig("plotdata.png")
-    plt.show()
-
-
 def plot_f(f, x, y, num=100):
     def separate(xs, ys):
         n = len(ys)
-        assert(len(xs) == n)
+        assert len(xs) == n
         x1p = [xs[i][0] for i in range(n) if ys[i] > 0]
         x2p = [xs[i][1] for i in range(n) if ys[i] > 0]
         x1m = [xs[i][0] for i in range(n) if ys[i] < 0]
@@ -99,36 +87,46 @@ def plot_f(f, x, y, num=100):
     ss = [10, 10, 30, 30]
     markers = ["x", "x", "o", "o"]
     labels = ["+1", "-1", "+1", "-1"]
-    plot_core(x1s, x2s, cs, ss, markers, labels)
+    for i in range(len(x1s)):
+        plt.scatter(x1s[i], x2s[i], c=cs[i], s=ss[i],
+                    marker=markers[i], label=labels[i])
+    plt.grid(True)
+    plt.legend(loc='upper left')
+    plt.savefig("plotdata.png")
+    plt.show()
 
 
 def load_x_y(fileName):
     x, y = [], []
-    with open(fileName, "r") as f:
-        for line in f.readlines():
-            xk = line.split(" ")
-            x.append([float(x) for x in xk[0:-1]])
-            y.append(float(xk[-1]))
+    try:
+        with open(fileName, "r") as f:
+            for line in f.readlines():
+                xk = line.split(" ")
+                x.append([float(x) for x in xk[0:-1]])
+                y.append(float(xk[-1]))
+    except:
+        print("invalid file " + fileName + "!!!")
+        exit(0)
     return x, y
 
 
 def parse_argv():
     __doc__ = """{f}
 Usage:
-    {f} <filename> [-m |--method <method>] [--plot]
+    {f} <filename> [-m | --method <method>] [--plot]
     {f} [-h | --help]
 Options:
-    -m --method              gauss(default), polynomial, sigmoid, linear
+    -m --method              {methods} (default:gauss)
     --plot                   show plotted graph (with matplotlib)
     -h --help                Show this help.
-""".format(f=__file__)
+""".format(f=__file__, methods=str(",".join(kernels.keys())))
     from docopt import docopt
     args = docopt(__doc__)
     if not args["<method>"]:
         args["<method>"] = "gauss"
     else:
         if args["<method>"] not in kernels:
-            print("method must be gauss, polynomial, sigmoid, or linear")
+            print("input valid method name!! ")
             exit(1)
     return args
 
