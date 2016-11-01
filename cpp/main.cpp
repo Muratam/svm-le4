964d641ccb65e0fe17c7d71e05256796c0500ab8
murata <murata@kmc.gr.jp>
2016-11-02 03:56:22 +0900

    cli

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,18 +3,17 @@
 using namespace std;
 
 int print_usage(char *const thisName) {
-  cout << "Usage :" << endl
-       << "    " << thisName
-       << " <dataname> [gauss| polynomial | linear] [--cross <div_num>]\n"
-          "            [--param <param>] [--plot <filename>.dat]\n"
-          "            [--C <param_C>] [--eps <param_eps>]"
-       << "Options :\n"
-       << "    --cross      crossvalidation :: div_num\n"
-       << "    --plot       plot function :: file name\n"
-       << "    --param      defined parameter :: parameter\n"
-       << "    --C          the parameter C\n"
-       << "    --eps        the parameter eps\n"
-       << endl;
+  cout << "Usage :\n"
+       << "  " << thisName
+       << " <dataname> [gauss| polynomial | linear] [--cross <div_num>] "
+          "[--param <param>] [--plot <filename>.dat] [--C <param_C>] [--eps "
+          "<param_eps>]\n"
+       << R"(Options :
+  --cross      crossvalidation :: div_num
+  --plot       plot function :: file name
+  --param      defined parameter :: parameter
+  --C          the parameter C  (default 1000)
+  --eps        the parameter eps(default 0.01))";
   return -1;
 }
 
@@ -28,19 +27,18 @@ int main(int argc, char *const argv[]) {
                PARAM_C = "--C", EPS = "--EPS";
   auto parsed = parse_args(args, {{CROSS, "10"},
                                   {PLOT, "result.dat"},
-                                  {PARAM, "10"},
-                                  {PARAM_C, "1000"},
-                                  {EPS, "0.01"}});
+                                  {PARAM, ""},
+                                  {PARAM_C, ""},
+                                  {EPS, ""}});
 
   const auto kernel_kind = Kernel::strings2kernel_kind(args);
   vector<vector<double>> x;
   vector<double> y;
   Kernel::read_data(argv[1], x, y);
   Kernel::normalize(x);
-  // TODO: C,epsのコマンドライン
-  double eps = atof(parsed[EPS].c_str());
+  double eps = parsed.count(EPS) ? atof(parsed[EPS].c_str()) : 1e-2;
   if (parsed.count(CROSS)) {  // 交差検定
-    auto pos = SVR::search_parameter(x, y, kernel_kind, eps, SVR::mean_square,
+    auto pos = SVR::search_parameter(x, y, kernel_kind, eps, SVR::coefficient,
                                      atoi(parsed[CROSS].c_str()));
     auto kernel2 = Kernel(kernel_kind, {pow(2.0, pos.p_center)});
     SVR svr(x, y, kernel2, pow(2.0, pos.c_center), eps);
@@ -52,7 +50,7 @@ int main(int argc, char *const argv[]) {
       svr.test(x, y);
     }
   } else {  // 普通にパラメータを指定して(プロット/テストする)
-    double C = atof(parsed[PARAM_C].c_str());
+    double C = parsed.count(PARAM_C) ? atof(parsed[PARAM_C].c_str()) : 1e3;
     auto kernel = Kernel(kernel_kind, {atof(parsed[PARAM].c_str())});
     SVR svr(x, y, kernel, C, eps);
     if (parsed.count(PLOT)) {
